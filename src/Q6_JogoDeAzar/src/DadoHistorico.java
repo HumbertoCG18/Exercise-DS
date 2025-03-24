b0ded102e11bdf94da24139e5f975899a7193eec
João Francisco S. Dallanora <joao55brdallas@gmail.com>
2025-03-24 18:45:06 -0300

    questao 6
    6

---
+++ src/Q6_JogoDeAzar/src/DadoHistorico.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class DadoHistorico implements DadoInterface {
+    private DadoInterface dado;
+    private ArrayList<Integer> historico;
+
+    public DadoHistorico(DadoInterface dado) {
+        this.dado = dado;
+        this.historico = new ArrayList<Integer>();
+    }
+
+    public void rolar() {
+        dado.rolar();
+        historico.add(getValor());
+    }
+
+    public int getValor() {
+        return dado.getValor(); // Retorna o valor do dado real
+    }
+
+    public ArrayList<Integer> getHistorico() {
+        return historico;
+    }
+}
